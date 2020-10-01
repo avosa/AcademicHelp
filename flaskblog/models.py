54b84786727f4637952fea2e6df7c480e456df3e
avosa <websterb17@gmail.com>
2020-10-01 13:49:06 +0300

    Modified few stuff

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flaskblog import db, login_manager
 from flask_login import UserMixin
-from flask import current_app,Markup
+from flask import current_app, Markup
 from micawber import bootstrap_basic, parse_html
 from markdown import markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
@@ -22,7 +22,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default-pic.jpg')
+    image_file = db.Column(db.String(20), nullable=False,
+                           default='default-pic.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -46,14 +47,15 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     category = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     @property
     def html_content(self):
-        hilite = CodeHiliteExtension( linenums=True, css_class='highlight')
+        hilite = CodeHiliteExtension(linenums=True, css_class='highlight')
         extras = ExtraExtension()
         markdown_content = markdown(self.content, extensions=[hilite, extras])
         oembed_content = parse_html(
@@ -61,9 +63,7 @@ class Post(db.Model):
             oembed_providers,
             urlize_all=True,
             maxwidth=800)
-        return Markup(oembed_content)    
-
+        return Markup(oembed_content)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}','{self.category}')"
-    
--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    content = TextAreaField('Description', validators=[DataRequired()])
     myChoices = [(1, 'Research Proposals and Theses'), (2, 'Linguistics'),
                  (3, 'Undergraduate Studies'), (4, 'General')]
     category = SelectField('Category', coerce=int,
